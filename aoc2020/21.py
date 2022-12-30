d3d4bdb2dedbd937ba81cf9602c4c30ee6c90f65
Guillaume <guillaume@local.host>
2022-12-30 21:00:10 +0100

    aoc2020 day 21

---
+++ aoc2020/21.py
@@ -0,0 +1,43 @@
+content = """mxmxvkd kfcds sqjhc nhms (contains dairy, fish)
+trh fvjkl sbzzf mxmxvkd (contains dairy)
+sqjhc fvjkl (contains soy)
+sqjhc mxmxvkd sbzzf (contains fish)
+"""
+
+with open("21.txt") as f:
+    content = f.read()
+
+
+def parse():
+    for line in content.splitlines():
+        ingredients, allergens = line.rstrip(")").split(" (contains ")
+        yield set(ingredients.split()), set(allergens.split(", "))
+
+
+pairs = tuple(parse())
+all_ingredients = set()
+all_allergens = set()
+for ingredients, allergens in pairs:
+    all_ingredients |= ingredients
+    all_allergens |= allergens
+
+safe_ingredients = set(all_ingredients)
+allergens_map = {}
+for allergen in all_allergens:
+    candidates = set(all_ingredients)
+    for ingredients, allergens in pairs:
+        if allergen in allergens:
+            candidates &= ingredients
+    safe_ingredients -= candidates
+    allergens_map[allergen] = candidates
+
+print(sum(len(ingredients & safe_ingredients) for ingredients, _ in pairs))
+
+while any(len(v) > 1 for v in allergens_map.values()):
+    for allergen, candidates in allergens_map.items():
+        if len(candidates) == 1:
+            for other_allergen, other_candidates in allergens_map.items():
+                if len(other_candidates) > 1:
+                    allergens_map[other_allergen] = other_candidates - candidates
+
+print(",".join(allergens_map[allergen].pop() for allergen in sorted(allergens_map)))
