608242915cd1ab1abd52e951038e6600a9495846
Guillaume Pujol <guillp.dev+github@pm.me>
2023-01-03 20:39:45 +0100

    aoc 2020 day 23

--- aoc2020/23.py
+++ aoc2020/23.py
@@ -1,32 +1,45 @@
-#cups = tuple(int(x) for x in "389125467")
+# cups = tuple(int(x) for x in "389125467")
 cups = tuple(int(x) for x in "123487596")
 
 
-def move(cups: tuple[int]) -> tuple[int]:
-    current_cup = cups[0:1]
-    pickedup_cups = cups[1:4]
-    destination_cup = (
-        sorted(cup for cup in cups[4:] if cup < current_cup[0])
-        or sorted(cup for cup in cups[4:] if cup > current_cup[0])
-    )[-1]
-    if destination_cup == cups[4]:
-        return cups[4:5] + pickedup_cups + cups[5:] + current_cup
-    else:
-        i = cups.index(destination_cup)
-        return cups[4 : i + 1] + pickedup_cups + cups[i + 1 :] + current_cup
+def crab(cups: tuple[int], n: int) -> tuple[int]:
+    ring = {a: b for a, b in zip(cups, cups[1:] + cups[:1])}
+
+    current_cup = cups[0]
+    for i in range(n):
+        pickedup_cups = (a := ring[current_cup], b := ring[a], c := ring[b])
+        destination_cup = current_cup
+        while destination_cup == current_cup or destination_cup in pickedup_cups:
+            destination_cup -= 1
+            if destination_cup == 0:
+                destination_cup = len(cups)
+        next_current_cup = ring[c]
+        ring[current_cup] = next_current_cup
+        ring[c] = ring[destination_cup]
+        ring[destination_cup] = a
+        current_cup = next_current_cup
+
+    def iter_ring(start):
+        yield start
+        n = ring[start]
+        while n != start:
+            yield n
+            n = ring[n]
+
+    return tuple(iter_ring(current_cup))
 
 
-cups1 = tuple(cups)
-for i in range(100):
-    cups1 = move(cups1)
-
-index1 = cups1.index(1)
-print("".join(str(c) for c in cups1[index1 + 1 :] + cups1[:index1]))
+def part1(cups: tuple[int]) -> str:
+    index1 = cups.index(1)
+    return "".join(str(x) for x in cups[:index1] + cups[index1 + 1 :])
 
 
-cups2 = cups + tuple(range(max(cups) + 1, 1000000))
+print(part1(crab(cups, 100)))
 
-for i in range(100):
-    index1 = cups2.index(1)
-    print(cups2[0:10], cups2[index1 : index1 + 3], cups2[-5:])
-    cups2 = move(cups2)
+
+def part2(cups: tuple[int]) -> int:
+    index1 = cups.index(1)
+    return cups[index1 + 1] * cups[index1 + 2]
+
+
+print(part2(crab(cups + tuple(range(max(cups) + 1, 1000000 + 1)), 10000000)))
