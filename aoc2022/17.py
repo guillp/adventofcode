926138212ee809a096e54dd3453be01c6128017b
Guillaume Pujol <guill.p.linux@gmail.com>
2023-01-08 19:10:33 +0100

    improved aoc 2020 day 17 part 1

--- aoc2022/17.py
+++ aoc2022/17.py
@@ -1,13 +1,10 @@
+from functools import cache
 from itertools import cycle
-
-import numpy as np
-import pandas as pd
 
 content = ">>><<><>><<<>><>>><<<>>><<<><<<>><>><<>>"
 
-with open("17.txt") as finput:
-    content = finput.read().strip()
-
+with open("17.txt") as f:
+    content = f.read().strip()
 
 ROCKS = """####
 
@@ -28,10 +25,32 @@ ROCKS = """####
 ##
 """
 
+
+@cache
+def move(rock: frozenset[complex], dir: complex):
+    return frozenset(pos + dir for pos in rock)
+
+
+def left(rock: frozenset[complex]) -> int:
+    return int(min(pos.real for pos in rock))
+
+
+def right(rock: frozenset[complex]) -> int:
+    return int(max(pos.real for pos in rock))
+
+
+def top(rock: frozenset[complex]) -> int:
+    return int(min(pos.imag for pos in rock))
+
+
+def bottom(rock: frozenset[complex]) -> int:
+    return int(max(pos.imag for pos in rock))
+
+
 rocks = [
-    pd.Series(
-        complex(x, y)
-        for y, line in enumerate(rock_str.splitlines())
+    frozenset(
+        complex(x + 2, -y - 4)  # adjusted so that rocks are at their starting positions
+        for y, line in enumerate(reversed(rock_str.splitlines()))
         for x, c in enumerate(line)
         if c == "#"
     )
@@ -40,54 +59,63 @@ rocks = [
 
 jets = cycle(content)
 
-chamber = pd.Series(complex(i) for i in range(7))
 
-
-def print_chamber(rock=pd.Series()):
-    c = {(k.real, k.imag): "#" if k.imag != 0 else "-" for k in chamber.values}
-    top = min(
-        int(chamber.apply(np.imag).min()),
-        int(rock.apply(np.imag).min()) if rock.any() else 1,
+def print_chamber(chamber: set[complex], rock: frozenset[complex] | None = None):
+    top_display = min(
+        top(chamber),
+        top(rock) if rock else 1,
     )
-    for y in range(top, 1):
+    for y in range(top_display, bottom(chamber)):
         print(
             "".join(
-                c.get((x, y), "@" if complex(x, y) in rock.values else ".")
+                "@"
+                if rock and complex(x, y) in rock
+                else "#"
+                if complex(x, y) in chamber
+                else "."
                 for x in range(7)
             )
         )
     print()
 
 
-for i in range(2022):
-    rock = rocks[i % len(rocks)].copy()
-    height = rock.apply(np.imag).max() - rock.apply(np.real).min() + 1
-    top = chamber.apply(np.imag).min()
-    rock += 2 + (top - 3 - height) * 1j
+def solve(n: int = 2022) -> int:
+    chamber = frozenset(complex(i) for i in range(7))
+    height = 0
+    states = {}
+    for i in range(n):
+        rock = rocks[i % len(rocks)]
 
-    while True:
-        push = next(jets)
-        if push == "<":
-            pushed_rock = rock - 1
+        # print_chamber(chamber, rock)
+        while True:
+            push = next(jets)
+            pushed_rock = move(rock, -1 if push == "<" else 1)
             if (
-                pushed_rock.apply(np.real).min() >= 0
-                and not pushed_rock.isin(chamber).any()
-            ):
-                rock = pushed_rock
-        elif push == ">":
-            pushed_rock = rock + 1
-            if (
-                pushed_rock.apply(np.real).max() < 7
-                and not pushed_rock.isin(chamber).any()
+                left(pushed_rock) >= 0
+                and right(pushed_rock) < 7
+                and not pushed_rock & chamber
             ):
                 rock = pushed_rock
 
-        fell_rock = rock + 1j
-        if not fell_rock.isin(chamber).any():
-            rock = fell_rock
-        else:
-            chamber = pd.concat((chamber, rock))
-            break
+            fell_rock = move(rock, 1j)
+            if not fell_rock & chamber:
+                rock = fell_rock
+            else:
+                break
+
+        chamber |= rock
+        # keep only the top part of the chamber
+        chamber = frozenset(pos for pos in chamber if pos.imag < 30)
+        # calc how much the chamber has raised
+        raised = -top(chamber)
+        height += raised
+        # put the chamber at 0 level again for the new rock
+        chamber = move(chamber, raised * 1j)
+
+        # print_chamber(chamber)
+
+    return height
+
 
 # print_chamber()
-print(-int(chamber.apply(np.imag).min()))
+print(solve(2022))
