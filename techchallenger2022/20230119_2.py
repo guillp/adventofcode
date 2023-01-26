0dd4493dde23cfe72e4a3568aeaa8a399b551c8e
Guillaume Pujol <guillp.dev+github@pm.me>
2023-01-26 10:37:23 +0100

    TechChallenger semifinal part 2

--- techchallenger2022/20230119_2.py
+++ techchallenger2022/20230119_2.py
@@ -4,17 +4,6 @@ squads = [int(input()) for _ in range(m)]
 
 target = (n + sum(squads)) // 2 - n + 1
 
-
-# def hire(squads: list[int], target: int, i: int = 0, total: int= 0) -> tuple[int, ...]:
-#     if total == target:
-#         return total
-#     if total >= target:
-#         yield total
-#     elif i < len(squads):
-#         yield from hire(squads, target, i + 1, total+squads[i])
-#         yield from hire(squads, target, i + 1, total)
-
-
 def hire(squads: list[int], target: int) -> int:
     totals = {0}
     for squad in squads:
@@ -24,6 +13,4 @@ def hire(squads: list[int], target: int) -> int:
     return min(total for total in totals if total >= target)
 
 
-squads.sort()
-best_groups = hire(squads, target)
-print(best_groups)
+print(hire(squads, target))
