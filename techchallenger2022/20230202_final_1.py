3069b36917bbc4de4380fb4318cb8a8c410f9929
Guillaume Pujol <guillaume.pujol@axa.com>
2023-02-03 14:58:56 +0100

    TechChallenger final part 1

---
+++ techchallenger2022/20230202_final_1.py
@@ -0,0 +1,25 @@
+import itertools
+
+n = int(input())
+conds = [
+    tuple(
+        (c, int(i) - 1)
+        for c, i in input().split()
+    )
+    for _ in range(n)
+]
+
+
+pool = set(itertools.product("RVB", repeat=5))
+
+for i, ((x, X), (y, Y), (z, Z)) in enumerate(conds):
+    for lights in list(pool):
+        if lights[X] == x or lights[Y] == y or lights[Z] == z:
+            continue
+        pool.remove(lights)
+
+    if not pool:
+        print(i)
+else:
+    print(i+1)
+
