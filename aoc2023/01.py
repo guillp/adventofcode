02e7faf243c3a0d375b424272f09e8c27d4f987d
Guillaume Pujol <guillp.dev+github@pm.me>
2023-12-01 21:33:31 +0100

    AoC 2023 day 01

--- aoc2023/01.py
+++ aoc2023/01.py
@@ -4,8 +4,8 @@ with open('01.txt') as f: content = f.read()
 
 s = 0
 for line in content.splitlines():
-    first = regex.search(r"(0|1|2|3|4|5|6|7|8|9)", line).group()
-    last = regex.search(r"(?r)(0|1|2|3|4|5|6|7|8|9)", line).group()
+    first = regex.search(r"(\d)", line).group()
+    last = regex.search(r"(?r)(\d)", line).group()
 
     s += 10 * int(first)
     s += int(last)
@@ -16,8 +16,8 @@ d = {"one": 1, "two": 2, "three": 3, "four": 4, "five": 5, "six": 6, "seven": 7,
 
 s2 = 0
 for line in content.splitlines():
-    first = regex.search(r"(one|two|three|four|five|six|seven|eight|nine|0|1|2|3|4|5|6|7|8|9)", line).group()
-    last = regex.search(r"(?r)(one|two|three|four|five|six|seven|eight|nine|0|1|2|3|4|5|6|7|8|9)", line).group()
+    first = regex.search(r"(one|two|three|four|five|six|seven|eight|nine|\d)", line).group()
+    last = regex.search(r"(?r)(one|two|three|four|five|six|seven|eight|nine|\d)", line).group()
 
     s2 += 10 * (d.get(first) or int(first))
     s2 += d.get(last) or int(last)
