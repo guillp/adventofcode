0311cc56571ede0c52ad63c216d93be05f059f1f
Guillaume <guill.p.linux@gmail.com>
2023-12-02 11:34:54 +0100

    AoC 2023 day 02

--- aoc2023/02.py
+++ aoc2023/02.py
@@ -1,34 +1,17 @@
+import re
+
 with open('02.txt') as f: content = f.read()
 
-s = 0
+s1 = s2 = 0
 for line in content.splitlines():
-    game_part, balls_part = line.split(":")
-    game_id = int(game_part.split(" ")[1])
+    game_id = int(re.findall(r'^Game (\d+):', line)[0])
 
-    for subset in balls_part.split(";"):
-        max_balls = {"red": 0, "blue": 0, "green": 0}
-        for ball in subset.split(","):
-            n, color = ball.strip().split(" ")
-            max_balls[color] = int(n)
+    red = max(int(x) for x in re.findall(r'(\d+) red', line))
+    green = max(int(x) for x in re.findall(r'(\d+) green', line))
+    blue = max(int(x) for x in re.findall(r'(\d+) blue', line))
+    if red <= 12 and green <= 13 and blue <= 14:
+        s1 += game_id
+    s2 += red*green*blue
 
-        if max_balls["red"] > 12 or max_balls["green"] > 13 or max_balls['blue'] > 14:
-            break
-    else:
-        s += game_id
-
-print(s)
-
-p = 0
-for line in content.splitlines():
-    game_part, balls_part = line.split(":")
-    game_id = int(game_part.split(" ")[1])
-    min_balls = {"red": 0, "blue": 0, "green": 0}
-
-    for subset in balls_part.split(";"):
-        for ball in subset.split(","):
-            n, color = ball.strip().split(" ")
-            if min_balls[color] < int(n):
-                min_balls[color] = int(n)
-    p += min_balls["red"] * min_balls["green"] * min_balls['blue']
-
-print(p)
+print(s1)
+print(s2)
