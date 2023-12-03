4e09e37bdfb89a6116762d34e84ff7a0fe3d34a7
Guillaume <guill.p.linux@gmail.com>
2023-12-03 17:18:39 +0100

    AoC 2023 day 03 optimization

--- aoc2023/03.py
+++ aoc2023/03.py
@@ -32,27 +32,21 @@ numbers = {
 
 
 def neighbors(x: int, y: int, l: int) -> Iterator[tuple[int, int]]:
-    # 3 neighbors on the left
-    yield x - 1, y - 1
-    yield x - 1, y
-    yield x - 1, y + 1
+    yield x - 1, y  # neighbor on the left
     # neighbors above and below
-    for d in range(l):
+    for d in range(-1, l + 1):
         yield x + d, y - 1
         yield x + d, y + 1
-    # 3 neighbors on the right
-    yield x + l, y - 1
-    yield x + l, y
-    yield x + l, y + 1
+    yield x + l, y  # neighbor on the right
 
 
 s = 0
-parts = set()
+parts = {}  # will store all parts, indexed by (y, x)
 for (x, y), num in numbers.items():
     for neighbor in neighbors(x, y, len(num)):
         if neighbor in symbols:
             s += int(num)
-            parts.add((x, y, num))
+            parts[y, x] = num
             break
 
 print(s)
@@ -64,10 +58,12 @@ for (xs, ys), symbol in symbols.items():
 
     adjacent_parts = set()
     nbs = tuple(neighbors(xs, ys, 1))
-    for xp, yp, num in parts:
+    for (yp, xp), num in parts.items():
+        if yp > ys + 1:  # avoid search parts that are below the symbol
+            break
         if any((xp + d, yp) in nbs for d in range(len(num))):
             adjacent_parts.add((num, xp, yp))
-            if len(adjacent_parts) > 2:
+            if len(adjacent_parts) > 2:  # avoid continuing the search when there are already 3 adjacent parts
                 break
 
     if len(adjacent_parts) == 2:
