02f455de4166db04d0a71491b21b67acd8c64b83
Guillaume Pujol <guillp.dev+github@pm.me>
2023-12-03 11:07:26 +0100

    AoC 2023 day 03 cleanup, no need for complex numbers

--- aoc2023/03.py
+++ aoc2023/03.py
@@ -15,60 +15,62 @@ content = """\
 
 with open('03.txt') as f: content = f.read()
 
+# extract each symbol from input, indexed by position
 symbols = {
-    complex(x, y): c
+    (x, y): c
     for y, line in enumerate(content.splitlines())
     for x, c in enumerate(line)
     if c != '.' and not c.isdigit()
 }
 
+# extract all numbers, indexed by position
+numbers = {
+    (match.start(), y): match.group()  # note: not converting to int here in case some number contain a leading 0
+    for y, line in enumerate(content.splitlines())
+    for match in re.finditer(r'\d+', line)
+}
 
-def read_numbers() -> Iterator[tuple[int, int, int, int]]:
-    for y, line in enumerate(content.splitlines()):
-        for match in re.finditer(r'\d+', line):
-            yield int(match.group()), match.start(), y, len(match.group())
 
-
-def neighbors(x: int, y: int, l: int) -> Iterator[complex]:
+def neighbors(x: int, y: int, l: int) -> Iterator[tuple[int, int]]:
     # 3 neighbors on the left
-    yield complex(x - 1, y - 1)
-    yield complex(x - 1, y)
-    yield complex(x - 1, y + 1)
+    yield x - 1, y - 1
+    yield x - 1, y
+    yield x - 1, y + 1
     # neighbors above and below
     for d in range(l):
-        yield complex(x + d, y - 1)
-        yield complex(x + d, y + 1)
+        yield x + d, y - 1
+        yield x + d, y + 1
     # 3 neighbors on the right
-    yield complex(x + l, y - 1)
-    yield complex(x + l, y)
-    yield complex(x + l, y + 1)
+    yield x + l, y - 1
+    yield x + l, y
+    yield x + l, y + 1
 
 
 s = 0
 parts = set()
-for number, x, y, l in read_numbers():
-    for neighbor in neighbors(x, y, l):
+for (x, y), num in numbers.items():
+    for neighbor in neighbors(x, y, len(num)):
         if neighbor in symbols:
-            s += number
-            parts.add((complex(x, y), l, number))
+            s += int(num)
+            parts.add((x, y, num))
             break
 
 print(s)
 
 s2 = 0
-for pos, symbol in symbols.items():
+for (xs, ys), symbol in symbols.items():
     if symbol != '*':
         continue
 
     adjacent_parts = set()
-    nbs = tuple(neighbors(int(pos.real), int(pos.imag), 1))
-    for origin, length, num in parts:
-        if any(origin + d in nbs for d in range(length)):
-            adjacent_parts.add((num, origin))
+    nbs = tuple(neighbors(xs, ys, 1))
+    for xp, yp, num in parts:
+        if any((xp + d, yp) in nbs for d in range(len(num))):
+            adjacent_parts.add((num, xp, yp))
             if len(adjacent_parts) > 2:
                 break
 
     if len(adjacent_parts) == 2:
-        s2 += adjacent_parts.pop()[0] * adjacent_parts.pop()[0]
+        s2 += int(adjacent_parts.pop()[0]) * int(adjacent_parts.pop()[0])
 
 print(s2)
