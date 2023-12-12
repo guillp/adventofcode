ba1276199dea6b55a50389047887821fce0cee45
Guillaume Pujol <guillp.dev+github@pm.me>
2023-12-12 15:26:32 +0100

    AoC 2023 day 12 optimization

--- aoc2023/12.py
+++ aoc2023/12.py
@@ -15,24 +15,30 @@ with open("12.txt") as f:
 
 @cache
 def solve(positions: str, groups: tuple[int]) -> int:
-    if not positions:
-        return 0 if groups else 1
-    if not groups:
-        return 0 if "#" in positions else 1
+    while positions and positions[0] == ".":  # remove unwanted leading dots
+        positions = positions[1:]
+
+    while positions and positions[-2:] == "..":  # remove trailing dots
+        positions = positions[:-1]
+
+    if positions and positions[-1] != ".":
+        positions += "."  # add an extra dot at the end to simplify our logic
+
+    if not groups and "#" not in positions:
+        return 1
+    if (
+        sum(groups) > len(positions)
+        or not groups and "#" in positions
+    ):
+        return 0 # fail fast on impossible cases
 
     s = 0
-    if positions[0] in ".?":  # if the group can't start here
-        s += solve(positions[1:], groups)
+    if "." not in positions[: groups[0]]:  # the next group fits
+        if positions[groups[0]] in ".?":  # there is a separator after the group
+            s += solve(positions[groups[0] + 1 :], groups[1:])
 
-    if positions[0] in "#?":
-        current_len = groups[0]
-        if current_len <= len(positions):  # can the current group fit?
-            if "." not in positions[:current_len]:
-                if "#" not in positions[current_len : current_len + 1]:
-                    s += solve(
-                        positions[current_len + 1 :],
-                        groups[1:],
-                    )
+    if positions[0] in ".?":  # try the same alg without the leading . or ?
+        s += solve(positions[1:], groups)
 
     return s
 
@@ -42,15 +48,16 @@ assert solve("?#?#?#?#?#?#?#?", (1, 3, 1, 6)) == 1
 assert solve("????.#...#...", (4, 1, 1)) == 1
 assert solve("????.######..#####.", (1, 6, 5)) == 4
 assert solve("?###????????", (3, 2, 1)) == 10
-s = 0
+
+part1 = 0
 for line in content.splitlines():
     positions, groups = line.split()
-    s += solve(positions, tuple(int(x) for x in groups.split(",")))
-print(s)
+    part1 += solve(positions, tuple(int(x) for x in groups.split(",")))
+print(part1)
 
-s = 0
+part2 = 0
 for line in content.splitlines():
     positions, groups = line.split()
     positions = "?".join(positions for _ in range(5))
-    s += solve(positions, tuple(int(x) for x in groups.split(",")) * 5)
-print(s)
+    part2 += solve(positions, tuple(int(x) for x in groups.split(",")) * 5)
+print(part2)
