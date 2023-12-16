a2ca480411d9e5e7902955f680419d14373d0bb5
Guillaume <guill.p.linux@gmail.com>
2023-12-16 13:16:13 +0100

    AoC 2023 day 16

---
+++ aoc2023/16.py
@@ -0,0 +1,117 @@
+from enum import Enum
+
+test_content = r"""
+.|...\....
+|.-.\.....
+.....|-...
+........|.
+..........
+.........\
+..../.\\..
+.-.-/..|..
+.|....-|.\
+..//.|....
+""".strip()
+
+with open('16.txt') as f: content = f.read()
+
+type Point = tuple[int, int]
+
+
+class Direction(Enum):
+    NORTH = 0, -1
+    SOUTH = 0, 1
+    EAST = 1, 0
+    WEST = -1, 0
+
+
+def continue_path(grid: dict[Point, str], cell: Point, direction: Direction) -> set[Point]:
+    paths = {(cell, direction)}
+    pool = {(cell, direction)}
+    while pool:
+        cell, direction = pool.pop()
+        x, y = cell
+        match direction:
+            case Direction.NORTH:
+                next_cell = (x, y - 1)
+            case Direction.SOUTH:
+                next_cell = (x, y + 1)
+            case Direction.EAST:
+                next_cell = (x + 1, y)
+            case Direction.WEST:
+                next_cell = (x - 1, y)
+            case _:
+                assert False
+
+        if (next_cell, direction) in paths:
+            continue  # avoid loops
+
+        match grid.get(next_cell):
+            case None:  # out of grid
+                continue
+            case "|":  # splitter
+                match direction:
+                    case Direction.EAST | Direction.WEST:
+                        pool |= {(next_cell, Direction.NORTH), (next_cell, Direction.SOUTH)}
+                    case Direction.NORTH | Direction.SOUTH:
+                        pool |= {(next_cell, direction)}
+            case "-":  # splitter
+                match direction:
+                    case Direction.NORTH | Direction.SOUTH:
+                        pool |= {(next_cell, Direction.EAST), (next_cell, Direction.WEST)}
+                    case Direction.EAST | Direction.WEST:
+                        pool |= {(next_cell, direction)}
+            case "\\":  # mirror
+                match direction:
+                    case Direction.NORTH:
+                        pool |= {(next_cell, Direction.WEST)}
+                    case Direction.SOUTH:
+                        pool |= {(next_cell, Direction.EAST)}
+                    case Direction.EAST:
+                        pool |= {(next_cell, Direction.SOUTH)}
+                    case Direction.WEST:
+                        pool |= {(next_cell, Direction.NORTH)}
+            case "/":  # mirror
+                match direction:
+                    case Direction.NORTH:
+                        pool |= {(next_cell, Direction.EAST)}
+                    case Direction.SOUTH:
+                        pool |= {(next_cell, Direction.WEST)}
+                    case Direction.EAST:
+                        pool |= {(next_cell, Direction.NORTH)}
+                    case Direction.WEST:
+                        pool |= {(next_cell, Direction.SOUTH)}
+            case ".":
+                pool |= {(next_cell, direction)}
+            case _:
+                assert False
+
+        paths.add((next_cell, direction))
+
+    return len({cell for cell, direction in paths})
+
+
+def part1(content: str) -> int:
+    grid = {(x, y): c for y, line in enumerate(content.splitlines()) for x, c in enumerate(line)}
+    return continue_path(grid, (0, 0), Direction.EAST)
+
+
+assert part1(test_content) == 46
+print(part1(content))
+
+
+def part2(content: str) -> int:
+    lines = content.splitlines()
+    H = len(lines)
+    W = len(lines[0])
+    grid = {(x, y): c for y, line in enumerate(lines) for x, c in enumerate(line)}
+    return max(
+        *(continue_path(grid, (x, 0), Direction.SOUTH) for x in range(W)),
+        *(continue_path(grid, (x, H), Direction.NORTH) for x in range(W)),
+        *(continue_path(grid, (0, y), Direction.EAST) for y in range(H)),
+        *(continue_path(grid, (W, y), Direction.WEST) for y in range(H)),
+    )
+
+
+assert part2(test_content) == 51
+print(part2(content))
