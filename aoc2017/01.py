3287ef9287a671d190f9bf160ba323c24423f5c8
Guillaume Pujol <guillp.dev+github@pm.me>
2024-05-06 09:47:16 +0200

    AoC 2017 days 1 to 4 reformatted

--- aoc2017/01.py
+++ aoc2017/01.py
@@ -1,5 +1,7 @@
-def solve(content: str, offset: int=1) -> int:
-    return sum(int(a) for a, b in zip(content, content[offset:] + content[:offset]) if a == b)
+def solve(content: str, offset: int = 1) -> int:
+    return sum(
+        int(a) for a, b in zip(content, content[offset:] + content[:offset]) if a == b
+    )
 
 
 assert solve("1122") == 3
@@ -13,6 +15,8 @@ assert solve("123425", 3) == 4
 assert solve("123123", 3) == 12
 assert solve("12131415", 4) == 4
 
-with open("01.txt") as f: content = f.read().strip()
+with open("01.txt") as f:
+    content = f.read().strip()
+
 print(solve(content))
 print(solve(content, len(content) // 2))
