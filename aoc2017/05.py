396877bb113969b6662f5b7835c4a3248b6c4f1f
Guillaume <guill.p.linux@gmail.com>
2024-05-14 08:30:53 +0200

    AoC 2017 day 5

---
+++ aoc2017/05.py
@@ -0,0 +1,39 @@
+test_content = """\
+0
+3
+0
+1
+-3
+"""
+
+def part1(content: str) -> int:
+    jumps = [int(x) for x in content.splitlines()]
+    i = step = 0
+    while 0 <= step < len(jumps):
+        s = jumps[step]
+        jumps[step] += 1
+        step += s
+        i += 1
+    return i
+
+
+def part2(content: str) -> int:
+    jumps = [int(x) for x in content.splitlines()]
+    i = step = 0
+    while 0 <= step < len(jumps):
+        s = jumps[step]
+        if s >= 3:
+            jumps[step] -= 1
+        else:
+            jumps[step] += 1
+        step += s
+        i += 1
+    return i
+
+assert part1(test_content) == 5
+assert part2(test_content)
+
+
+with open("05.txt") as f: content = f.read()
+print(part1(content))
+print(part2(content))
