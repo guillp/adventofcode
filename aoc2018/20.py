364d4c90cb00bfdaebe297eac296b886a4bcfcbb
Guillaume Pujol <guillp.dev+github@pm.me>
2024-07-23 19:44:28 +0200

    AoC 2018 days 18-21 and 23

---
+++ aoc2018/20.py
@@ -0,0 +1,56 @@
+from collections import defaultdict
+
+DIRECTIONS = {"N": -1j, "E": 1, "S": 1j, "W": -1}
+
+
+def solve(content: str) -> tuple[int, int]:
+    pos = 0j
+    G: dict[complex, set[complex]] = defaultdict(set)
+    levels = {}
+    level = 0
+    for c in content:
+        match c:
+            case "^" if G:
+                assert False, "Unexpected ^"
+            case "(":
+                level += 1
+                levels[level] = pos
+            case ")":
+                pos = levels[level]
+                level -= 1
+            case "|":
+                pos = levels[level]
+            case "$" if level != 0:
+                assert False, "Unexpected $"
+            case _ if c in DIRECTIONS:
+                d = DIRECTIONS[c]
+                G[pos].add(pos + d)
+                G[pos + d].add(pos)
+                pos += d
+
+    pool: list[tuple[int, tuple[complex, ...]]] = [(0, (0j,))]
+    longest = 0
+    more_than_1000 = set()
+    while pool:
+        dist, path = pool.pop()
+        longest = max(longest, dist)
+        pos = path[-1]
+        if dist >= 1000:
+            more_than_1000.add(pos)
+        for next_pos in G[pos]:
+            if next_pos in path:
+                continue  # don't walk twice in the same room
+            pool.append((dist + 1, path + (next_pos,)))
+
+    return longest, len(more_than_1000)
+
+
+assert solve("^WNE$") == (3, 0)
+assert solve("^ENWWW(NEEE|SSE(EE|N))$") == (10, 0)
+assert solve("^ENNWSWW(NEWS|)SSSEEN(WNSE|)EE(SWEN|)NNN$") == (18, 0)
+assert solve("^ESSWWN(E|NNENN(EESS(WNSE|)SSS|WWWSSSSE(SW|NNNE)))$") == (23, 0)
+assert solve("^WSSEESWWWNW(S|NENNEEEENN(ESSSSW(NWSW|SSEN)|WSWWN(E|WWS(E|SS))))$") == (31, 0)
+
+with open("20.txt") as f:
+    content = f.read()
+print(*solve(content), sep="\n")
---
+++ aoc2018/23.py
@@ -0,0 +1,63 @@
+import re
+from collections.abc import Iterator
+
+from z3 import Bool, If, Int, Ints, Optimize  # type: ignore[import-not-found]
+
+
+def solve(content: str) -> Iterator[int]:
+    def iter_bots() -> Iterator[tuple[tuple[int, int, int], int]]:
+        for line in content.strip().splitlines():
+            x, y, z, r = map(int, re.findall(r"-?\d+", line))
+            yield (x, y, z), r
+
+    bots = dict(iter_bots())
+    nbots = len(bots)
+
+    strongest = max(bots, key=lambda bot: bots[bot])
+    rng = bots[strongest]
+
+    yield sum(sum(abs(a - b) for a, b in zip(bot, strongest)) <= rng for bot in bots)
+
+    # z3 to the rescue
+    o = Optimize()
+    x, y, z = Ints("x y z")
+
+    def Abs(x: Int) -> If:
+        return If(x >= 0, x, -x)
+
+    is_in_range = [Bool(f"in_range{i}") for i in range(nbots)]
+
+    for i, ((bx, by, bz), rng) in enumerate(bots.items()):
+        o.add(is_in_range[i] == (Abs(x - bx) + Abs(y - by) + Abs(z - bz) <= rng))
+    bots_in_range = Int("n")
+    o.add(bots_in_range == sum(is_in_range))
+
+    distance = Int("d")
+    o.add(distance == Abs(x) + Abs(y) + Abs(z))
+
+    o.maximize(bots_in_range)
+    o.minimize(distance)
+
+    assert o.check()
+    m = o.model()
+    yield abs(m[x].as_long()) + abs(m[y].as_long()) + abs(m[z].as_long())
+
+
+test_content = """\
+pos=<0,0,0>, r=4
+pos=<1,0,0>, r=1
+pos=<4,0,0>, r=3
+pos=<0,2,0>, r=1
+pos=<0,5,0>, r=3
+pos=<0,0,3>, r=1
+pos=<1,1,1>, r=1
+pos=<1,1,2>, r=1
+pos=<1,3,1>, r=1
+"""
+
+assert tuple(solve(test_content)) == (7, 1)
+
+with open("23.txt") as f:
+    content = f.read()
+for part in solve(content):
+    print(part)
