0cbda71091c3bfd73631edb79b018e39afd7612f
Guillaume <guill.p.linux@gmail.com>
2024-09-17 21:51:11 +0200

    fix AoC 2016 day 5

--- aoc2016/05.py
+++ aoc2016/05.py
@@ -1,11 +1,10 @@
 import hashlib
+from functools import cache
 
 
+@cache
 def md5hash(door_id: str, i: int) -> str:
     return hashlib.md5((door_id + str(i)).encode()).hexdigest()
-
-
-hash_cache = {}
 
 
 def part1(door_id: str) -> str:
@@ -15,42 +14,22 @@ def part1(door_id: str) -> str:
         md5 = md5hash(door_id, i)
         if md5.startswith("00000"):
             p += md5[5]
-            # print(p)
-            hash_cache[i] = md5
         i += 1
 
     return p
 
 
-def get_char(md5: str) -> tuple[int, str]:
-    if md5.startswith("00000") and md5[5].isdigit():
-        n = int(md5[5])
-        if 0 <= n < 8:
-            return n, md5[6]
-    raise ValueError
-
-
 def part2(door_id: str) -> str:
     p: list[str | None] = [None] * 8
-    for i, md5 in hash_cache.items():
-        try:
-            n, c = get_char(md5)
-            if p[n] is None:
-                p[n] = c
-        except ValueError:
-            pass
-    # print("".join(p[n] or "." for n in range(8)))
+    i = 0
     while None in p:
-        i += 1
         md5 = md5hash(door_id, i)
-        try:
-            n, c = get_char(md5)
-            if p[n] is None:
+        if md5.startswith("00000") and md5[5].isdigit():
+            n = int(md5[5])
+            if 0 <= n < 8 and p[n] is None:
+                c = md5[6]
                 p[n] = c
-                # print("".join(p[n] or "." for n in range(8)))
-        except ValueError:
-            pass
-
+        i += 1
     return "".join(p)  # type: ignore[arg-type]
 
 
