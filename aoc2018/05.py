a03070f913275b0b9c0cfff05ba2338c58c2c164
Guillaume <guill.p.linux@gmail.com>
2024-10-16 15:39:43 +0200

    AoC 2018 day 5 faster

--- aoc2018/05.py
+++ aoc2018/05.py
@@ -1,39 +1,32 @@
-import string
-from itertools import pairwise
+from collections.abc import Iterator
 
 
-def part1(content: str) -> int:
-    d = dict(enumerate(content))
-
-    while d:
-        for i, j in pairwise(d.keys()):
-            a = d[i]
-            b = d[j]
-            if a.islower() and b.isupper() and a.upper() == b or a.isupper() and b.islower() and a.lower() == b:
-                del d[i]
-                del d[j]
-                break
+def react(polymer: bytes) -> int:
+    i = 0
+    while 0 <= i < len(polymer) - 1:
+        left, right = polymer[i : i + 2]
+        if abs(left - right) == 32:
+            polymer = polymer[:i] + polymer[i + 2 :]
+            if i > 0:
+                i -= 1
         else:
-            break
-
-    return len(d)
-
-
-def part2(content: str) -> int:
-    def check(letter: str) -> int:
-        return part1(content.replace(letter.lower(), "").replace(letter.upper(), ""))
-
-    return min(check(letter) for letter in string.ascii_lowercase)
+            i += 1
+    return len(polymer)
 
 
-test_content = "dabAcCaCBAcCcaDA"
+def solve(content: str) -> Iterator[int]:
+    polymer = content.strip().encode()
+    yield react(polymer)
+    part2 = len(polymer)
+    for letter in "abcdefghijklmnopqrstuvwxyz":
+        part2 = min(part2, react(polymer.replace(letter.lower().encode(), b"").replace(letter.upper().encode(), b"")))
+    yield part2
 
-assert part1(test_content) == 10
-assert part2(test_content) == 4
 
+assert tuple(solve("dabAcCaCBAcCcaDA")) == (10, 4)
 
 with open("05.txt") as f:
     content = f.read()
 
-print(part1(content))
-print(part2(content))
+for part in solve(content):
+    print(part)
