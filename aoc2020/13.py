750c7189ccaac0ef5ad0739889139787fe70930a
Guillaume Pujol <guill.p.linux@gmail.com>
2024-11-27 18:16:56 +0100

    AoC 2020 days 13 and 15

---
+++ aoc2020/13.py
@@ -0,0 +1,22 @@
+import re
+
+
+def part1(content: str):
+    earliest, *buses = (int(x) for x in re.findall(r"\d+", content, re.MULTILINE))
+    bus = min(buses, key=lambda b: -earliest % b)
+    return bus * (-earliest % bus)
+
+def part2(content: str):
+    buses = tuple((int(x), i) for i, x in enumerate(content.splitlines()[1].split(",")) if x != "x")
+    departure, step = 0, 1
+    for bus, offset in buses:
+        while (departure + offset) % bus:
+            departure += step
+        step *= bus
+    return departure
+
+with open("13.txt") as f:
+    content = f.read()
+
+print(part1(content))
+print(part2(content))
