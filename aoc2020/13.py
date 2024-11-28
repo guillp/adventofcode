8b1b0bb2bdd356ad3b186171c839cc6950ca34db
Guillaume <guill.p.linux@gmail.com>
2024-11-28 21:18:24 +0100

    linting

--- aoc2020/13.py
+++ aoc2020/13.py
@@ -1,12 +1,13 @@
 import re
 
 
-def part1(content: str):
+def part1(content: str) -> int:
     earliest, *buses = (int(x) for x in re.findall(r"\d+", content, re.MULTILINE))
     bus = min(buses, key=lambda b: -earliest % b)
     return bus * (-earliest % bus)
 
-def part2(content: str):
+
+def part2(content: str) -> int:
     buses = tuple((int(x), i) for i, x in enumerate(content.splitlines()[1].split(",")) if x != "x")
     departure, step = 0, 1
     for bus, offset in buses:
@@ -14,6 +15,7 @@ def part2(content: str):
             departure += step
         step *= bus
     return departure
+
 
 with open("13.txt") as f:
     content = f.read()
