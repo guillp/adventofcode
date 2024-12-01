9414a0808d08278c8e4af61c96c68fd3aa5d4d62
Guillaume Pujol <guillp.dev+github@pm.me>
2024-12-01 08:29:46 +0100

    AoC2024 day 1

---
+++ aoc2024/01.py
@@ -0,0 +1,28 @@
+import re
+from collections.abc import Iterator
+
+
+def solve(content: str) -> Iterator[int]:
+    numbers = [int(x) for x in re.findall(r"\d+", content, re.MULTILINE)]
+    left_list = sorted(numbers[::2])
+    right_list = sorted(numbers[1::2])
+
+    yield sum(abs(left - right) for left, right in zip(left_list, right_list))
+    yield sum(left * right_list.count(left) for left in left_list)
+
+
+test_content = """\
+3   4
+4   3
+2   5
+1   3
+3   9
+3   3
+"""
+
+assert tuple(solve(test_content)) == (11, 31)
+with open("01.txt") as f:
+    content = f.read()
+
+for part in solve(content):
+    print(part)
