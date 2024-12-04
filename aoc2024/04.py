bb271893bc99b79bd6990aa8fd56f061bfde817c
Guillaume Pujol <guillp.dev+github@pm.me>
2024-12-04 07:27:29 +0100

    AoC 2024 day 4

---
+++ aoc2024/04.py
@@ -0,0 +1,48 @@
+from itertools import product
+
+
+def solve(content: str) -> tuple[int, int]:
+    lines = content.strip().splitlines()
+    height = len(lines)
+    width = len(lines[0])
+    assert height == width
+    grid = {complex(x, y): c for y, line in enumerate(lines) for x, c in enumerate(line)}
+    part1 = part2 = 0
+    for x, y in product(range(height), repeat=2):
+        pos = complex(x, y)
+        for direction in (-1j, 1 - 1j, 1, 1 + 1j, 1j, -1 + 1j, -1, -1 - 1j):
+            if (
+                grid.get(pos) == "X"
+                and grid.get(pos + direction) == "M"
+                and grid.get(pos + 2 * direction) == "A"
+                and grid.get(pos + 3 * direction) == "S"
+            ):
+                part1 += 1
+
+        if grid.get(pos) == "A" and {grid.get(pos - 1 - 1j), grid.get(pos + 1 + 1j)} == {
+            grid.get(pos + 1 - 1j),
+            grid.get(pos - 1 + 1j),
+        } == {"M", "S"}:
+            part2 += 1
+    return part1, part2
+
+
+test_content = """\
+MMMSXXMASM
+MSAMXMSMSA
+AMXSXMAAMM
+MSAMASMSMX
+XMASAMXAMM
+XXAMMXXAMA
+SMSMSASXSS
+SAXAMASAAA
+MAMMMXMMMM
+MXMXAXMASX
+"""
+assert solve(test_content) == (18, 9)
+
+with open("04.txt") as f:
+    content = f.read()
+
+for part in solve(content):
+    print(part)
